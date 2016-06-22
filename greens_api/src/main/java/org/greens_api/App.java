24fc79ea6d6814fb77c9d18c8a28827f480ebdda
yekailoveqq <yekailoveqq@163.com>
2016-06-22 16:12:58 +0800

    添加后端服务包

---
+++ greens_api/src/main/java/org/greens_api/App.java
@@ -0,0 +1,13 @@
+package org.greens_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
