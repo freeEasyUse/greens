88aa71736919edde27769040510aa7fa1e66e3cf
yekailoveqq <yekailoveqq@163.com>
2016-06-23 13:52:39 +0800

    duboo测试 jetty服务

--- greens_rest/src/main/java/org/greens/controller/DubboController.java
+++ greens_rest/src/main/java/org/greens/controller/DubboController.java
@@ -1,10 +1,17 @@
 package org.greens.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.greens.api.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 
@@ -23,9 +30,13 @@ public class DubboController extends BaseController {
 	@Qualifier(value="dubboServiceImple")
 	private DubboService dubboService;
 	
-	@RequestMapping("/sayHello")
-	public void sayHelloWithDubbo(){
-		dubboService.sayHelloDubbo("good");
+	@RequestMapping(value = "/sayHello",method = {RequestMethod.GET})
+	@ResponseBody
+	public void sayHelloWithDubbo(HttpServletResponse response){
+		String res = dubboService.sayHelloDubbo("good");
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("data", res);
+		returnSuccess(response, result);
 	}
 		
 }
