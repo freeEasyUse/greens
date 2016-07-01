5d44bab8290397d35c2256b1a81e61432b54c7e4
yekailoveqq <yekailoveqq@163.com>
2016-07-01 17:36:41 +0800

    foot

---
+++ greens_web/src/main/webapp/model_js/footCommon.js
@@ -0,0 +1,83 @@
+/**
+ * 底部组件 
+ */
+
+/** 帮助中心 */
+var HelpCenter = React.createClass({
+	render:function(){
+		return (
+			<div className="col-md-3 cust">
+					<h4>新手上路</h4>
+					<li><a href="#">帮助中心</a></li>
+					<li><a href="#">常见问题</a></li>
+					<li><a href="buy.html">购物流程</a></li>
+					<li><a href="#">退货说明</a></li>
+			</div>
+		);
+	}
+});
+
+/** 关于我们 */
+var AboutUs = React.createClass({
+	render:function(){
+		return (
+			<div className="col-md-2 abt">
+				<h4>关于我们</h4>
+				<li><a href="#">关于菜蓝</a></li>
+				<li><a href="#">监控中心</a></li>
+				<li><a href="#">招聘英才</a></li>
+				<li><a href="contact.html">联系我们</a></li>
+			</div>
+		);
+	}
+});
+
+/** 会员中心 */
+var UserCenter =  React.createClass({
+	render:function(){
+		return (
+			<div className="col-md-2 myac">
+				<h4>会员中心</h4>
+				<li><a href="register.html">登录说明</a></li>
+				<li><a href="#">我的信息</a></li>
+				<li><a href="#">订单记录</a></li>
+				<li><a href="buy.html">支付情况</a></li>
+			</div>
+		);
+	}
+});
+
+
+/** 地址 */
+var Address = React.createClass({
+	render:function(){
+		return (
+			<div className="col-md-5 our-st">
+				<h4>OUR STORES</h4>
+				<li><i class="add"> </i>Jl. Haji Muhidin, Blok G no.69</li>
+				<li><i class="phone"> </i>025-2839341</li>
+				<li><a href="mailto:info@example.com"><i class="mail"> </i>info@sitename.com </a></li>
+			</div>
+		);
+	}
+});
+
+
+var FootComment =  React.creatClass({
+	render:function(){
+		return (
+				<div className="container">
+					<HelpCenter/>
+					<AboutUs/>
+					<UserCenter/>
+					<UserCenter/>
+				</div>
+		);
+	}
+});
+
+ReactDOM.render(<FootComment/>,$("#footer").get(0));
+
+
+
+
