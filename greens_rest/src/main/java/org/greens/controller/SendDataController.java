02cee6178f1523d7baf96b84612cf25e1de950b4
yekailoveqq <yekailoveqq@163.com>
2016-07-05 15:13:55 +0800

    动态获取菜单和商品信息

---
+++ greens_rest/src/main/java/org/greens/controller/SendDataController.java
@@ -0,0 +1,69 @@
+package org.greens.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/sendData")
+@Controller
+public class SendDataController extends BaseController {
+
+	/**
+	 * 返回菜单数据
+	 * @param response
+	 */
+	@RequestMapping("/menu")
+	@ResponseBody
+	public void sendMenuData(HttpServletResponse response){
+		List<Map<String, Object>> menus = new ArrayList<Map<String,Object>>();
+		for(int i = 0;i<5;i++){
+			Map<String, Object> menu = new HashMap<String, Object>();
+			menu.put("showText", i+"showText");
+			List<Map<String, Object>> lists = new ArrayList<Map<String,Object>>();
+			for(int j = 0;j<6;j++){
+				Map<String, Object> d = new HashMap<String, Object>();
+				d.put("title", j+"title");
+				List<Map<String, Object>> datas = new ArrayList<Map<String,Object>>();
+				for(int k = 0;k<6;k++){
+					Map data = new HashMap<String, Object>();
+					data.put("name", k+"name");
+					data.put("code", "code"+k);
+					datas.add(data);
+				}
+				d.put("list", datas);
+				lists.add(d);
+			}
+			menu.put("list", lists);
+			menus.add(menu);
+		}
+		Map<String, Object> r = new HashMap<String, Object>();
+		r.put("result", menus);
+		returnSuccess(response, r);
+	}
+	
+	
+	@RequestMapping("/goods")
+	@ResponseBody
+	public void sendGoodsData(HttpServletResponse response, @RequestParam("menuCode")String menuCode){
+		List<Map<String, Object>> goodGroup = new ArrayList<Map<String,Object>>();
+		int dex = Integer.parseInt(menuCode.substring(0, 1))*4+4;
+		for(int i = 0;i<dex;i++){
+			Map<String, Object> o = new HashMap<String, Object>();
+			o.put("imgSrc", "images/w5.jpg");
+			o.put("goodName", "goodName"+i);
+			o.put("desc", "description");
+			goodGroup.add(o);
+		}
+		Map<String, Object> r = new HashMap<String, Object>();
+		r.put("result", goodGroup);
+		returnSuccess(response, r);
+	}
+}
--- greens_web/src/main/webapp/model_js/megaMenuCommon.js
+++ greens_web/src/main/webapp/model_js/megaMenuCommon.js
@@ -2,16 +2,17 @@
  * megamenu组建
  */
 
-/** 每列显示组件 ul li*/
+/** 每列显示组件 ul li */
 var ListData = React.createClass({
 	render:function(){
+		console.log("render listData");
 		return (
 				<div className="col1">
 					<div className="h_nav">
 						<h4>{this.props.data.title}</h4>
 						<ul>
-							{this.props.data.list.map(function(v){			//循环属性
-								return <li><a href="#">{v.name}</a></li>
+							{this.props.data.list.map(function(v){			// 循环属性
+								return <li><a href="#" data={v.name}>{v.name}</a></li>
 							})}
 						</ul>
 					</div>
@@ -24,6 +25,7 @@ var ListData = React.createClass({
 /** 创建显示面板 megapanel */
 var ShowPanel = React.createClass({
 	render:function(){
+		console.log("render ShowPanel");
 		return (
 				<div className="megapanel">
 					<div className="row">
@@ -37,15 +39,41 @@ var ShowPanel = React.createClass({
 });
 
 
-/** 创建 megamenu*/
+/** 创建 megamenu */
 var MegaMenu = React.createClass({
+	
+	getInitialState:function(){
+		console.log("getInitiaState");
+		var result;
+		$.ajax({
+  			url: this.props.url,
+  			async: false,
+  			dataType: 'json',
+  			contentType : "application/json",
+  			type: 'POST',
+  			success: function(data) {
+  				console.log("ajax request");
+				var r = $.parseJSON(data);
+				result = r;
+				console.log(result);
+  			}.bind(this),
+  			error: function(xhr, status, err) {
+    			console.error(this.props.addUrl, status, err.toString());
+  			}.bind(this)
+		});
+		return result;
+	},
+	
+	
 	render:function(){
+		console.log("render MegaMenu");
+		console.log(this.state.result);
 		return (
 			<ul className="megamenu skyblue">
 				<li className="active grid"><a className="color1" href="index.html">index</a></li>
-				{this.props.liListData.map(function(liv){
+				{this.state.result.map(function(liv){
 					console.log("xunhuan2");
-					console.log(liv.list);
+					console.log(liv.showText);
 					return(
 							<li>
 								<a className="color1" href="#">{liv.showText}</a>
@@ -57,12 +85,127 @@ var MegaMenu = React.createClass({
 		);
 	},
 	componentDidMount:function(){
-		console.log("componetn");
+		console.log("compoentDidMount");
 		$(".megamenu").megamenu();
+		console.log("find a");
+		console.log($(".h_nav >ul > li > a"));
+		$(".h_nav >ul > li > a").click(function(event){
+			var aTag = $(event.target).get(0);
+			var menuCode = $(aTag).attr("data");
+			$.ajax({
+	  			url: "/greens_web/sendData/goods?menuCode="+menuCode,
+	  			async: false,
+	  			dataType: 'json',
+	  			contentType : "application/json",
+	  			type: 'GET',
+	  			success: function(data) {
+					var r = $.parseJSON(data);
+					ReactDOM.render(<ItemGroup goodGroup={r.result}/>,$("#goodGroup").get(0));
+	  			}.bind(this),
+	  			error: function(xhr, status, err) {
+	    			console.error(this.props.addUrl, status, err.toString());
+	  			}.bind(this)
+			});
+		});
 	}
 });
 
 
+		/**
+		 * 内容区组件
+		 */
+
+		/** 具体商品显示组件 */
+		var GoodItemShow = React.createClass({
+			render:function(){
+				return (
+					<div className="grid1_of_4">
+						<div className="content_box">
+							<a href="details.html">
+								<img src={this.props.item.imgSrc} className="img-responsive" alt=""/>
+							</a>
+							<h4><a href="details.html">{this.props.item.goodName}</a></h4>
+							<p>{this.props.item.desc}</p>
+							<div className="grid_1 simpleCart_shelfItem">
+								<div className="item_add"><span className="item_price"><h6>ONLY $109.00</h6></span></div>
+								<div className="item_add"><span className="item_price"><a href="#">add to cart</a></span></div>
+							</div>
+						</div>
+					</div>
+				);
+			}
+		});
+
+
+
+
+
+		/** 商品组显示 */
+		var ItemGroup = React.createClass({
+			render:function(){
+				var groups = this.props.goodGroup;
+				var allItems = new Array();
+				for(var i = 0;i<groups.length;i++){
+					if(i%4==0){
+						var showGroups = new Array();
+						for(var j = 0;j<4;j++){
+							if((i+j)<groups.length){
+								showGroups.push(<GoodItemShow item={groups[i+j]}/>);
+							}
+							else{
+								break;
+							}
+							
+						}
+						allItems.push(<div className="grids_of_4">{showGroups}</div>)
+					}
+				}
+				allItems.push(<div class="clearfix"></div>);
+				return (
+						<div>
+							{allItems}
+						</div>
+				);
+			}
+		});
+
+
+
+		var goodGroup = new Array();
+		var good = new Object();
+		good.imgSrc = "images/w5.jpg";
+		good.goodName = "goodName";
+		good.desc = "Itsdfafasdfasdfasdf";
+
+		var good2 = new Object();
+		good2.imgSrc = "images/w5.jpg";
+		good2.goodName = "goodName";
+		good2.desc = "Itsdfafasdfasdfasdf";
+
+		goodGroup.push(good);
+		goodGroup.push(good2);
+
+		for(var i = 0;i<12;i++){
+			var o = new Object();
+			o.imgSrc = "images/w5.jpg";
+			o.goodName = "goodName";
+			o.desc = "Itsdfafasdfasdfasdf";
+			goodGroup.push(o);
+		}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 var d = new Object();
 var a = new Array();
 var l1 = new Object();
@@ -107,4 +250,4 @@ mlist.push(o1);
 
 
 
-ReactDOM.render(<MegaMenu liListData={mlist}/>,$("#menu").get(0));
+ReactDOM.render(<MegaMenu url={"/greens_web/sendData/menu"}/>,$("#menu").get(0));
